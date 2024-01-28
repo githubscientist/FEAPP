87411cf9b632b98fb61050c9835475b78e8de19a
githubscientist <sathish.datascientist@gmail.com>
2024-01-28 12:40:09 +0530

    base url updated

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiService = axios.create({
-    baseURL: "http://localhost:3001/api",
+    baseURL: "https://placesapp-be.onrender.com/api/",
 });
 
 export default apiService;
